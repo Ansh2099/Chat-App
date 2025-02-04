b6e58beac2519768454ae34aac825ced4a5f36ad
Ansh2099 <anshpreetsingh1009@gmail.com>
2025-02-04 21:08:59 +0530

    Added Event Disconnent Listener

--- Chat-App/src/main/java/com/ansh/Chat/App/Config/WebSocketEventListener.java
+++ Chat-App/src/main/java/com/ansh/Chat/App/Config/WebSocketEventListener.java
@@ -1,4 +1,34 @@
 package com.ansh.Chat.App.Config;
 
+import com.ansh.Chat.App.Controller.MessageType;
+import com.ansh.Chat.App.Model.Message;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+@Component
+@Slf4j
 public class WebSocketEventListener {
+    
+    private SimpMessageSendingOperations messagingTemplate;
+    
+    @EventListener
+    public void sessionDisconnectEvent(SessionDisconnectEvent event) {
+
+        String username = event.getUser().getName();
+        log.info("User Disconnected : " + username);
+        
+        if (username != null){
+
+            Message message = new Message();
+
+            message.setType(MessageType.LEAVE);
+            message.getContent();
+            message.setSender(username);
+
+            messagingTemplate.convertAndSend("/topic/public", message);
+        }
+    }
 }
--- Chat-App/src/main/java/com/ansh/Chat/App/Model/Message.java
+++ Chat-App/src/main/java/com/ansh/Chat/App/Model/Message.java
@@ -6,12 +6,35 @@ import lombok.*;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-@Getter
-@Setter
 @Builder
 public class Message {
 
     private String content;
+
+    public MessageType getType() {
+        return Type;
+    }
+
+    public void setType(MessageType type) {
+        Type = type;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
     private MessageType Type;
     private String sender;
 }
